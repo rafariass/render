8a9b8096cd4a40390a18b12084ad55a9d5178905
Raul Farias S <ra.farias.s@outlook.com>
2023-11-21 13:41:48 -0300

    error log 404

--- backend/src/server/controllers/errors.controller.js
+++ backend/src/server/controllers/errors.controller.js
@@ -1,4 +1,4 @@
 import HTTP_STATUS from '../../config/constants.js'
 
 export const notFound = (_, res) =>
-  res.status(HTTP_STATUS.not_found.code).json({ code: HTTP_STATUS.not_found.code, message: HTTP_STATUS.not_found.text })
+  res.status(HTTP_STATUS.not_found.code).json({ code: HTTP_STATUS.not_found.code, message: 'Web no encontrada' })
