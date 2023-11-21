ce28b8c3e60e9c91c672eca335c01b7fd266bdc7
Raul Farias S <ra.farias.s@outlook.com>
2023-11-21 12:30:46 -0300

    chore: initialize project

---
+++ backend/src/server/database/db.js
@@ -0,0 +1,27 @@
+import pkg from 'pg'
+
+const { Pool } = pkg
+
+const config = {
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DATABASE,
+  allowExitOnIdle: true
+  // ssl: {
+  //   rejectUnauthorized: true
+  // }
+}
+
+const pool = new Pool(config)
+
+const db = (query, values) => pool
+  .query(query, values)
+  .then(({ rows }) => rows)
+  .catch(({ code, message }) => {
+    const error = { status: '[ERROR]', code, message }
+    throw error
+  })
+
+export default db
---
+++ frontend/src/config/constans.js
@@ -0,0 +1,6 @@
+export const URLBASE = import.meta.env.VITE_URL_BACKEND
+
+export const ENDPOINT = {
+  login: `${URLBASE}/login`,
+  users: `${URLBASE}/usuarios`
+}
